a0d259671314bde9a5b7d995a06289e86424b675
Heather Hern <heathernewlin1@gmail.com>
2020-10-22 19:36:13 -0400

    Added package.json

--- app.js
+++ app.js
@@ -5,42 +5,39 @@ var inquirer = require("inquirer");
 
 var connection = mysql.createConnection({
     host: "localhost",
-
     port: 3306,
-
     user: "root",
-
     password: "",
     database: "employee_trackerDB"
 });
 
 connection.connect(function (err) {
     if (err) throw err;
-    runSearch();
+    // questions();
 });
 
-// function songSearch() {
-//     inquirer
-//         .prompt({
-//             name: "song",
-//             type: "input",
-//             message: "What song would you like to look for?"
-//         })
-//         .then(function (answer) {
-//             console.log(answer.song);
-//             connection.query("SELECT * FROM top5000 WHERE ?", { song: answer.song }, function (err, res) {
-//                 if (err) throw err;
-//                 console.log(
-//                     "Position: " +
-//                     res[0].position +
-//                     " || Song: " +
-//                     res[0].song +
-//                     " || Artist: " +
-//                     res[0].artist +
-//                     " || Year: " +
-//                     res[0].year
-//                 );
-//                 runSearch();
-//             });
-//         });
-// }
+function questions() {
+    inquirer
+        .prompt({
+            name: "song",
+            type: "input",
+            message: "What song would you like to look for?"
+        })
+        .then(function (answer) {
+            console.log(answer.song);
+            connection.query("SELECT * FROM top5000 WHERE ?", { song: answer.song }, function (err, res) {
+                if (err) throw err;
+                console.log(
+                    "Position: " +
+                    res[0].position +
+                    " || Song: " +
+                    res[0].song +
+                    " || Artist: " +
+                    res[0].artist +
+                    " || Year: " +
+                    res[0].year
+                );
+                runSearch();
+            });
+        });
+}
